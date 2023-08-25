bcf07b9324282cea5737e342a123afdc434f8596
Joabe <joabeo@gmail.com>
2023-08-24 22:57:43 -0300

    Voltando a url para local

--- src/app/api-service.js
+++ src/app/api-service.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const httpClient = axios.create({
-//    baseURL: 'http://localhost:8080'
-    baseURL: 'https://joabefinancas-api-67f1c8df9a65.herokuapp.com/'
+    baseURL: 'http://localhost:8080'
+//    baseURL: 'https://joabefinancas-api-67f1c8df9a65.herokuapp.com/'
     
 })
 
